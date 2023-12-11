89c0e1f3a564c3c036593f70b6f796a6bc97f2f8
ROSHAN-27 <86292504+ROSHAN-27@users.noreply.github.com>
2023-12-11 23:33:30 +0530

    first commit

---
+++ javascript/image.js
@@ -0,0 +1,15 @@
+const sliderImg = document.getElementById('imageChange');
+
+function slider() {
+    let i =1;
+    setInterval(()=>{        
+            sliderImg.src= `../images/sliderImage${i}.png`
+            console.log(`../images/sliderImage${i}.png`)
+            if(i>=3) {
+                i=1;
+            } else {
+                i++;
+            }
+              
+    }, 3000)
+}
